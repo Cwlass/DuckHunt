d78db987ae86c9bfe4d5d2a57a5d0c8ef7db38ca
danyklein7554 <62357139+danyklein7554@users.noreply.github.com>
2021-02-24 12:13:44 +0100

    img - css v4

--- scriptFront.js
+++ scriptFront.js
@@ -1,11 +1,14 @@
 
 document.getElementById("Set").addEventListener("click", openDiv);
 function openDiv() {
-  document.querySelector(".SetBanner").style.display = "block";
+  document.querySelector(".SetBanner").style.display = "flex";
+  document.querySelector(".SetBanner").style.opacity = "100%";
+  document.querySelector(".SetBanner").style.left = "50%";
 }
 
 document.getElementById("SetClose").addEventListener("click", CloseDiv);
 function CloseDiv() {
-  document.querySelector(".SetBanner").style.display = "none";
+  document.querySelector(".SetBanner").style.left = "200%";
+  
 }
 

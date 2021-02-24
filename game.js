850d77b08f09bf6679cb2c0efcd4d0dc0780f4eb
Cwlass <39944635+Cwlass@users.noreply.github.com>
2021-02-24 15:54:45 +0100

    Fixed death bug

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 //Variable intialisation
+const gameOver = document.querySelector('.gameOver');
+const gameSpace = document.querySelector('.gameSpace');
 let shooterScore = 0;
 let birdDead = false;
 let duckLeft = 50;
@@ -8,6 +10,7 @@ let up = false;
 let left = false;
 let right = false;
 let bullets = 3;
+let canMove = true;
 duck = document.querySelector('.duck');
 //Directoinal Control
 document.addEventListener("keydown", function (event) {
@@ -50,6 +53,7 @@ document.addEventListener("keyup", function (event) {
 
 //Bird Shooting
 document.querySelector('.duck').addEventListener("click", function () {
+    canMove = false;
     shooterScore++;
     document.querySelector('.score').innerHTML = shooterScore;
     duck.style.backgroundImage = 'url(img/deadDuck.png)';
@@ -57,13 +61,13 @@ document.querySelector('.duck').addEventListener("click", function () {
         birdDead = true;
         duck.style.backgroundImage = 'url(img/duckdead.png)';
     }, 500);
-    document.querySelector(".Dog").style.bottom = "0%";
+    document.querySelector(".NiceShot").style.bottom = "0%";
 })
 
 
 // All movement handling
 let t = setInterval(function () {
-    if (!birdDead) {
+    if (!birdDead && canMove) {
         if (up && duckBottom < 95) { duckBottom++; }
         if (down && duckBottom > 5) { duckBottom--; }
         if (left && duckLeft > 5) {
@@ -77,28 +81,18 @@ let t = setInterval(function () {
         duck.style.bottom = duckBottom + '%';
         duck.style.left = duckLeft + '%';
     } else {
-        duckBottom--
+        if (duckBottom > 5) {
+            duckBottom--
+        }
         duck.style.bottom = duckBottom + '%';
         if (duckBottom <= 5) {
             duck.style.transform = "rotate(90deg)"
             birdDead = false;
             setTimeout(function () {
                 resetBird();
-            }, 500);
+            }, 1000);
         }
     }
-}, 20);
-
-function resetBird() {
-    duckBottom = 50;
-    duckLeft = 50;
-    duck.style.backgroundImage = "url(img/Duck_Hunt-logo-8044A0A3B6-seeklogo.com.png)"
-    duck.style.transform = "rotate(0deg)"
-}
-
-document.querySelector(".gameSpace").addEventListener('click', function () {
-    console.log("Bang");
-    bullets--
     if (bullets == 2) {
         document.querySelector(".Bullet1").style.display = "none";
     }
@@ -110,8 +104,39 @@ document.querySelector(".gameSpace").addEventListener('click', function () {
         document.querySelector('.gameOver').style.display = "flex";
 
     }
+}, 20);
+
+function resetBird() {
+    duckBottom = 50;
+    duckLeft = 50;
+    duck.style.backgroundImage = "url(img/Duck_Hunt-logo-8044A0A3B6-seeklogo.com.png)"
+    duck.style.transform = "rotate(0deg)"
+    document.querySelector(".NiceShot").style.bottom = "-200%";
+    canMove = true;
+}
+
+document.querySelector(".gameSpace").addEventListener('click', function () {
+    console.log("Bang");
+    bullets--
 })
 
+gameOver.querySelector('p').addEventListener('click', resetGame);
+
+function resetGame() {
+    bullets = 3;
+    document.querySelector(".Bullet1").style.display = "block";
+    document.querySelector(".Bullet2").style.display = "block";
+    document.querySelector(".Bullet3").style.display = "block";
+    document.querySelector(".BShot").style.display = "flex";
+    if (shooterScore > document.querySelector(".Score").innerHTML) {
+        document.querySelector(".Score").innerHTML = shooterScore;
+    }
+
+    shooterScore = 0;
+    gameOver.style.display = "none";
+    gameSpace.style.display = "none";
+    document.querySelector(".HomeBanner").style.display = "block";
+}
 
 
 // Dany //
